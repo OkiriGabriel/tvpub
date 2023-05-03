518148aa2aab046e949e499bd3b88bf17dd0184d
portable-ewaade <portableewaade@gmail.com>
2023-05-03 17:06:01 +0100

    updated

--- src/pages/movies/my-movies.js
+++ src/pages/movies/my-movies.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from '../../components/Navbar'
+import { useDisclosure } from '@mantine/hooks';
 import Button from '../../components/Button'
 import { Form } from 'react-bootstrap'
 import { Container, Row, Col } from "react-bootstrap";
 import MoviesCard from './MoviesCard';
+import UploadModal from "./uploadModal";
 
 const myMovies = () => {
+  const [showComponent, setShowComponent] = useState(false);
+  const [opened, { open, close }] = useDisclosure(false);
+  function handleButtonClick(event) {
+    event.preventDefault();
+    setShowComponent(true);
+    console.log('kk')
+  }
+
+
+
     const projects = [
         {
           title: "White wedding",
@@ -69,52 +81,48 @@ const myMovies = () => {
         },
         
       ];
-
-
-
-
-
-
-    return (
-        <div className='my-movies'>
-        <Navbar />
-        <div className='container mt-4'>
-        <Form className="d-flex pt-2 ">
-            <input type='search' placeholder="Search for a show or movie" className='input' />
-            <img src='../images/search.png' className='search-icon' />
-            <Button className='fs-small search-btn'>
-                <img src='../images/upload-videp.png' className='upload-icon' alt='upload icon' />
-                Upload Video</Button>
-                <Button className='fs-small search-btn'>
-                <img src='../images/upload-videp.png' className='upload-icon' alt='upload icon' />
-                Hero Video</Button>
-            </Form>
-        </div>
-
-        <Container fluid className='movie-container'>
-        <div className="mt-4">
-        <Row>
-          <Col size={12}>
-                  <Row >
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <MoviesCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                </Row>
-          </Col>
-        </Row>
-        </div>
-        </Container>
      
 
+
+
+      return (
+        <div className='my-movies'>
+          <Navbar />
+          <div className='container mt-4'>
+            <Form className="d-flex pt-2 ">
+              <input type='search' placeholder="Search for a show or movie" className='input' />
+              <img src='../images/search.png' className='search-icon' />
+              {showComponent && <UploadModal   opened={opened} onClose={close}  />}
+<Button className='fs-small search-btn' onClick={open}>
+  <img src='../images/upload-videp.png' className='upload-icon' alt='upload icon'/>
+  Upload Video
+</Button>
+    
+              <Button className='fs-small search-btn'>
+                <img src='../images/upload-video.png' className='upload-icon' alt='upload icon' />
+                Hero Video
+              </Button>
+            </Form>
+          </div>
+    
+          <Container fluid className='movie-container'>
+            <div className="mt-4">
+              <Row>
+                <Col size={12}>
+                  <Row >
+                    {projects.map((project, index) => (
+                      <MoviesCard key={index} {...project} />
+                    ))}
+                  </Row>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+    
+         
         </div>
-    )
+      )
+    
 }
 
 export default myMovies
---
+++ src/pages/movies/uploadModal.js
@@ -0,0 +1,54 @@
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button, Group, TextInput, Textarea } from '@mantine/core';
+import { IconChevronDown } from '@tabler/icons-react';
+
+
+function UploadModal() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Drawer position='right' opened={opened} onClose={close} className="side-modal">
+        {/* Drawer content */} 
+            <div className='text-center'>
+                <h6 className='text-center mb-3'>Upload Videos</h6>
+                <img src='../images/video-uplooad.png' alt='upload-video img' className='w-25 mb-1' />
+                <a href="#" className='primary-color fs-small fw-light'>
+                    <p>Add cover image</p>
+                </a>
+            </div>
+
+        <div className=''>
+        <TextInput label="Movie Title" placeholder="Tap to edit" />
+        <TextInput label="Movie Creator" placeholder="Tap to edit" />
+        <TextInput label="Staring" placeholder="Tap to edit" />
+        <Textarea label="Movie Description" placeholder="Tap to edit" />
+
+        <TextInput component="select" label="Movie Genre" placeholder='Select from dropdown' rightSection={<IconChevronDown size={14} stroke={1.5} />}>
+            <option value="Select from dropdown">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+        </TextInput>
+
+        <TextInput component="select" label="Age restriction " placeholder='Select from dropdown' rightSection={<IconChevronDown size={14} stroke={1.5} />}>
+            <option value="Select from dropdown">Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+        </TextInput>
+        
+        
+        <Button type="submit" className="my-button-modal fs-small">
+            <img src='../images/upload-videp.png' className='upload-icon'/> Send Step </Button>
+        <Button type="submit" className="my-button-modal2 fs-small"> Send Step </Button>
+        </div>
+
+      </Drawer>
+
+      <Group position="center">
+        <Button onClick={open}>Open Drawer</Button>
+      </Group>
+    </>
+  );
+}
+
+export default UploadModal
