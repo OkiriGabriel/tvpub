a8c760b2ba5a6f4cf0844c5ec6c3dcd87442830e
portable-ewaade <portableewaade@gmail.com>
2023-05-05 19:10:33 +0100

    update

--- src/pages/movies/inviteModal.js
+++ src/pages/movies/inviteModal.js
@@ -8,6 +8,8 @@ function InviteModal() {
     <>
       <Modal opened={opened} onClose={close} title="Invite Account" centered>
         <TextInput label="Email Address" placeholder="Tap to edit" />
+
+        <Button className='fs-small my-button-modal2'>Invite User</Button>
         
       </Modal>
 
